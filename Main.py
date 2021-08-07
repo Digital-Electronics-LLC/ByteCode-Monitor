89b4ae993c2a7226f0224a79557cdc99f609954a
DiamondCoder1000 <78707555+DiamondCoder1000@users.noreply.github.com>
2021-08-06 19:08:09 -0700

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,10 @@
+print("SSG CPython Compiler")
+print("Translate Python to Bytecode")
+print("Type 'q' to quit")
+import dis
+while True:
+  x = input()
+  if x == "q":
+    break
+  else:
+    dis.dis(x)
